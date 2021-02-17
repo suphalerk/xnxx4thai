e82c69c28e29459db4f6f3881b821ed8e4c84246
James <64403343+jokerjames001@users.noreply.github.com>
2021-02-17 11:52:34 +0700

    info

--- components/Info.tsx
+++ components/Info.tsx
@@ -22,99 +22,96 @@ export default function AutoGrid() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3} style={{ marginLeft: '-10% !important', marginTop: '1% !important'  }}>
-        <Grid item xs>
-          <Paper className={classes.paper}></Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}><p className="text-green">ตอบโจทย์ทุกการซื้อขายกับเรา <img className="small-logo" src="/images/logo1.png" /></p>
+    <div className={classes.root} style={{backgroundColor:'#f2f2f2', marginTop:'1em'}}>
+      <Grid container spacing={4}>
+        
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <p className="text-green2">ตอบโจทย์ทุกการซื้อขายกับเรา <img className="small-logo" src="/images/logo1.png" /></p>
           </Paper>
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}></Paper>
-        </Grid>
+       
       </Grid>
-      <Grid container spacing={3} style={{ marginLeft: '-10% !important' }}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            <p className="text-header2">ผู้ขาย<Button className="btn-sell2">ลงขาย</Button></p>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              ขั้นตอนง่ายๆ ใครก็ลงขายได้
-            </Typography>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              โปรโมทขายเพื่อเพิ่มโอกาศในการขายมากขึ้น
-            </Typography>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              แชทกับลูกค้าโดยตรงเพื่อเพิ่มโอกาสในการปิดการขาย
-            </Typography>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              มีระบบหลังบ้านในการจัดการรายการเพื่อโปรโมทสิ้นค้าได้ง่าย
-            </Typography>
-          </Paper>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+        <Paper className={classes.paper}>
+    <p className="text-header2">ผู้ขาย<Button className="btn-sell2">ลงขาย</Button></p>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      ขั้นตอนง่ายๆ ใครก็ลงขายได้
+    </Typography>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      โปรโมทขายเพื่อเพิ่มโอกาศในการขายมากขึ้น
+    </Typography>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      แชทกับลูกค้าโดยตรงเพื่อเพิ่มโอกาสในการปิดการขาย
+    </Typography>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      มีระบบหลังบ้านในการจัดการรายการเพื่อโปรโมทสิ้นค้าได้ง่าย
+    </Typography>
+  </Paper>
         </Grid>
-        <Grid item xs>
+        <Grid item xs={6}>
           <Paper className={classes.paper}>
-            <p className="text-header2" >ผู้ซื้อ<Button className="btn-sell3">ลงขาย</Button></p>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              สะดวกมั่นใจในการเลือกซื้อสินค้าที่หลากหลาย
-            </Typography>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              พูดคุย สอบถามกับคนขายได้โดยตรงเพื่อความปรอดภัยในการซื้อ
-            </Typography>
-            <Typography color={"primary"} >
-            <Image
-              className="img-correct"
-              src="/images/CORRECT.png"
-              alt=""
-              width={20}
-              height={20}
-            />
-              มีาทงเลือกมากมาย กับ ร้านค้าและสินค้ามากมายอัพเดทตลอดเวลา
-            </Typography>
-          </Paper>
+          <p className="text-header2" >ผู้ซื้อ<Button className="btn-sell3">ลงขาย</Button></p>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      สะดวกมั่นใจในการเลือกซื้อสินค้าที่หลากหลาย
+    </Typography>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      พูดคุย สอบถามกับคนขายได้โดยตรงเพื่อความปรอดภัยในการซื้อ
+    </Typography>
+    <Typography color={"primary"} >
+    <Image
+      className="img-correct"
+      src="/images/CORRECT.png"
+      alt=""
+      width={20}
+      height={20}
+    />
+      มีาทงเลือกมากมาย กับ ร้านค้าและสินค้ามากมายอัพเดทตลอดเวลา
+    </Typography>
+  </Paper>
         </Grid>
       </Grid>
     </div>
