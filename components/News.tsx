ac2b88d246f0f4b0640e53385f3a318ebd420bad
James <64403343+jokerjames001@users.noreply.github.com>
2021-02-17 16:07:07 +0700

    Update News.tsx

--- components/News.tsx
+++ components/News.tsx
@@ -5,7 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
-
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
