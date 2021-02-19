4ac682a14d8ac0c59c1ac2e76322121f31fd251f
James <64403343+jokerjames001@users.noreply.github.com>
2021-02-19 10:35:41 +0700

    fix css layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,9 +19,9 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
     </Head>
     <header>
       <nav>
-        <img className="logo1" src="/images/logo1.png" width='220px' height='auto' /> 
-        <img className="btn-love" src="/images/love.png" />
-        <img className="btn-chat" src="/images/chat2.png" />
+        <img className="logo1" src="/images/logo1.png" width='220px' height='51.45' /> 
+        <img className="btn-love" src="/images/love.png"  width='31px' height='25'/>
+        <img className="btn-chat" src="/images/chat2.png" width='35px' height='31' />
 
        <a className="text-login" href="Prodict"> เข้าสู่ระบบ / สมัครสมาชิก</a>
          
