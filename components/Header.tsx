5aced282ff4ad1e968174cf94155ff81702407ae
James <64403343+jokerjames001@users.noreply.github.com>
2021-02-19 11:27:54 +0700

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import Image from "next/image";
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,34 +13,51 @@ const useStyles = makeStyles((theme: Theme) =>
     paper: {
       padding: theme.spacing(2),
       color: theme.palette.text.secondary,
-      backgroundColor:'#fff',
+      backgroundColor: "#fff",
       boxShadow: "none",
     },
-    logo:{
-        float:'left',
-        textAlign:"left",
+    paper2: {
+      backgroundColor: "#fff",
+      boxShadow: "none",
+      padding: theme.spacing(2),
+      textAlignLast: "center",
     },
-    love:{
-        float:'right',
-        textAlign:"right",
+    paper3: {
+      backgroundColor: "#fff",
+      boxShadow: "none",
+      padding: theme.spacing(2),
+      textAlignLast: "right",
     },
-    chat:{
-        float:'right',
-        textAlign:"right",
+    paper4: {
+      backgroundColor: "#fff",
+      boxShadow: "none",
+      padding: theme.spacing(2),
+      textAlignLast: "left",
     },
-    btnSell:{
-        float:'right',
-        marginLeft:'1em',
-        color:'#fff',
-        backgroundColor:'#00C900'
+    logo: {
+      // float:'left',
+      textAlignLast: "center",
     },
-    btnSell2:{
-        float:'right',
-        marginLeft:'1em',
-        color:'#00C900'
-    }
-
-  }),
+    love: {
+      float: "right",
+      textAlign: "right",
+    },
+    chat: {
+      float: "right",
+      textAlign: "right",
+    },
+    btnSell: {
+      //   float: "right",
+      marginLeft: "1em",
+      color: "#fff",
+      backgroundColor: "#00C900",
+    },
+    btnSell2: {
+      //   float: "right",
+      marginLeft: "1em",
+      color: "#00C900",
+    },
+  })
 );
 
 export default function AutoGrid() {
@@ -50,45 +67,51 @@ export default function AutoGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs lg={3}>
-          <Paper className={classes.paper}>
-              <Image
+          <Paper className={classes.paper2}>
+            <Image
               className={classes.logo}
               src="/images/logo1.png"
               alt=""
               width={"170"}
               height={"45"}
-              />
-          </Paper>
-        </Grid>
-        <Grid item xs lg={5}>
-          <Paper className={classes.paper}>
-
-             
+            />
           </Paper>
         </Grid>
         <Grid item xs lg={3}>
-          <Paper className={classes.paper}>
-          {/* <Image
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+        <Grid item xs lg={1}>
+          <Paper className={classes.paper3}>
+            <Image
               className={classes.love}
               src="/images/love.png"
               alt=""
-              width={"30"}
-              height={"30"}
-              />
-               <Image
-              className={classes.chat}
+              width={"35"}
+              height={"35"}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs lg={1}>
+          <Paper className={classes.paper4}>
+            <Image
+              className={classes.love}
               src="/images/chat2.png"
               alt=""
               width={"35"}
               height={"35"}
-              /> */}
-               <Button className={classes.btnSell} variant="contained">ลองขาย</Button>
-              <Button  className={classes.btnSell2} color="primary">เข้าสู่ระบบ / สมัครสมาชิก</Button>
-             
+            />
           </Paper>
         </Grid>
-       
-       
+        <Grid item xs lg={3}>
+          <Paper className={classes.paper}>
+            <Button className={classes.btnSell2} color="primary">
+              เข้าสู่ระบบ / สมัครสมาชิก
+            </Button>
+            <Button className={classes.btnSell} variant="contained">
+              ลองขาย
+            </Button>
+          </Paper>
+        </Grid>
       </Grid>
     </div>
   );
