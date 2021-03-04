9efbf8db76c8354dccb1e91c6cf30ecbdc115e3c
James <64403343+jokerjames001@users.noreply.github.com>
2021-03-04 13:31:03 +0700

    Update index.tsx

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import PicList from '../PicList'
 import ProductDetail from '../ProductDetail'
 import Announcemobile from '../Announcemobile'
+import Search2 from '../Search2'
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -116,59 +118,9 @@ const Product = (props: any) => {
 
   return (
     <Layout title="Product | KaideeWa.com">
+      <Search2/>
       <Container maxWidth="lg" className="container">
         <div className={classes.root}>
-          {/* <Grid container spacing={3}>
-        <Grid item xs={4} lg={3}>
-          <Paper className={classes.paper2}>
-            <Image
-              className={classes.logo}
-              src="/images/logo1.png"
-              alt=""
-              width={"170"}
-              height={"45"}
-            />
-          </Paper>
-        </Grid>
-        <Grid item xs lg={3} className={classes.paper5}>
-          <Paper className={classes.paper5}>
-
-          </Paper>
-        </Grid>
-        <Grid item xs lg={1} className={classes.gridDis}>
-          <Paper className={classes.paper3}>
-            <Image
-              className={classes.love}
-              src="/images/love.png"
-              alt=""
-              width={"35"}
-              height={"35"}
-            />
-          </Paper>
-        </Grid>
-        <Grid item xs lg={1} >
-          <Paper className={classes.paper4}>
-            <Image
-              className={classes.love}
-              src="/images/chat2.png"
-              alt=""
-              width={"35"}
-              height={"35"}
-            />
-          </Paper>
-        </Grid>
-        <Grid item xs lg={3}>
-          <Paper className={classes.paper}>
-
-            <Link href="/about">
-          <a className={classes.btnRegister}>เข้าสู่ระบบ / สมัครสมาชิก</a>
-        </Link>
-            <Button className={classes.btnSell} variant="contained">
-              ลองขาย
-            </Button>
-          </Paper>
-        </Grid>
-      </Grid>   */}
           {/* <Search2/>  */}
           <PicList />
           <ProductDetail />
