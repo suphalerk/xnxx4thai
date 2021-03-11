758ac456a1a3189fe071d40e70a33a5bfb29b14e
James <64403343+jokerjames001@users.noreply.github.com>
2021-03-11 14:44:33 +0700

    Update Searchfilter.tsx

--- components/Searchfilter.tsx
+++ components/Searchfilter.tsx
@@ -145,29 +145,24 @@ const FullWidthGrid = (props: { keywords: Keyword[] }) => {
         </IconButton> */}
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+        <Grid container spacing={1} className={classes.gridCon}>
+        {keywordFilter.map((k, index) => {
+          if (k == undefined) return <></>
+
+          return <Grid item xs={4} md={3} sm={3} lg={2} key={index}>
+            <Paper className={classes.paper}>
+              <Image
+                className="img-category"
+                src={k.image}
+                alt=""
+                width={75}
+                height={75}
+              />
+              <p className={classes.textLink}>{k.name}</p>
+            </Paper>
+          </Grid >
+        })}
+      </Grid>
         </CardContent>
       </Collapse>
     
