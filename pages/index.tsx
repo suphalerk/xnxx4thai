3c775b2637e0b50cf519eefb766382446b03021d
James <64403343+jokerjames001@users.noreply.github.com>
2021-03-17 11:43:51 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from 'next'
 import Home from '../components/Home'
 import Products from '../src/products/Products'
 
+
 export const getServerSideProps: GetServerSideProps = async (context) => await Products(context)
 
 export default Home
