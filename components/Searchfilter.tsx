03de1648231de231dee170c0e751d2e3db8157ce
James <64403343+jokerjames001@users.noreply.github.com>
2021-03-22 15:36:54 +0700

    Update Searchfilter.tsx

--- components/Searchfilter.tsx
+++ components/Searchfilter.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Image from 'next/image'
-import { Keyword } from '../interfaces/product'
-import { CardContent, Collapse, Grid, Paper } from '@material-ui/core';
-import clsx from 'clsx';
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Image from "next/image";
+import { Keyword } from "../interfaces/product";
+import { CardContent, Collapse, Grid, Link, Paper } from "@material-ui/core";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,95 +12,148 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     root: {
       flexGrow: 1,
-     
-
     },
     paper: {
       padding: theme.spacing(2),
-      textAlign: 'center',
+      textAlign: "center",
       color: theme.palette.text.secondary,
-      backgroundColor: '#fff',
-      boxShadow: 'none',
-
+      backgroundColor: "#fff",
+      boxShadow: "none",
     },
     textLink: {
-      color: '#111',
-      fontSize: '14px'
+      color: "#111",
+      fontSize: "14px",
     },
     gridCon: {
-      display: 'flex',
+      display: "flex",
     },
     more: {
-      color: '#00C900',
+      color: "#00C900",
     },
     btnMore: {
-      margin: '-4px 4px !important',
-      width: '17px',
-      height: 'auto',
+      margin: "-4px 4px !important",
+      width: "17px",
+      height: "auto",
     },
-     heading: {
+    heading: {
       fontSize: theme.typography.pxToRem(15),
       fontWeight: theme.typography.fontWeightRegular,
     },
-    AccordionSum:{
-      placeContent:'center',
+    AccordionSum: {
+      placeContent: "center",
     },
     expand: {
-      color:'#00C900',
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
+      color: "#00C900",
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
         duration: theme.transitions.duration.shortest,
       }),
     },
     expandOpen: {
-      transform: 'rotate(0deg)',
+      transform: "rotate(0deg)",
     },
-  }),
+  })
 );
 
 interface KeywordData {
-  name: string,
-  data: string[]
-  image: string
+  name: string;
+  data: string[];
+  image: string;
 }
 
 const keywordTypes: KeywordData[] = [
-  { name: "อสังหาริมทรัพย์", data: ["บ้าน", "ที่ดิน"], image: "/images/Real-estate2.png" },
+  {
+    name: "อสังหาริมทรัพย์",
+    data: ["บ้าน", "ที่ดิน"],
+    image: "/images/Real-estate2.png",
+  },
   { name: "รถยนต์", data: ["รถยนต์", "รถบรรทุก"], image: "/images/car2.png" },
-  { name: "มอเตอร์ไซค์", data: ["มอเตอร์ไซค์"], image: "/images/Motocycle2.png" },
-  { name: "มือถือ แท๊บเล็ต", data: ["มือถือ แท๊บเล็ต", "samsung", "xiaomi", "oppo", "Huawei", "OnePlus", "Vivo"], image: "/images/Mobile-tablet2.png" },
-  { name: "คอมพิวเตอร์", data: ["คอมพิวเตอร์"], image: "/images/Computer2.png" },
-  { name: "เครื่องใช้ไฟฟ้า", data: ["เครื่องใช้ไฟฟ้า"], image: "/images/Electrical2.png" },
+  {
+    name: "มอเตอร์ไซค์",
+    data: ["มอเตอร์ไซค์"],
+    image: "/images/Motocycle2.png",
+  },
+  {
+    name: "มือถือ แท๊บเล็ต",
+    data: [
+      "มือถือ แท๊บเล็ต",
+      "samsung",
+      "xiaomi",
+      "oppo",
+      "Huawei",
+      "OnePlus",
+      "Vivo",
+    ],
+    image: "/images/Mobile-tablet2.png",
+  },
+  {
+    name: "คอมพิวเตอร์",
+    data: ["คอมพิวเตอร์"],
+    image: "/images/Computer2.png",
+  },
+  {
+    name: "เครื่องใช้ไฟฟ้า",
+    data: ["เครื่องใช้ไฟฟ้า"],
+    image: "/images/Electrical2.png",
+  },
   { name: "สัตว์เลี้ยง", data: ["สัตว์เลี้ยง"], image: "/images/Pet2.png" },
   { name: "พระเครื่อง", data: ["พระเครื่อง"], image: "/images/Amulets2.png" },
-  { name: "บ้านและสวน", data: ["บ้านและสวน"], image: "/images/Home-garden2.png" },
-  { name: "อะไหล่ยนต์", data: ["อะไหล่ยนต์"], image: "/images/Car-accessories2.png" },
+  {
+    name: "บ้านและสวน",
+    data: ["บ้านและสวน"],
+    image: "/images/Home-garden2.png",
+  },
+  {
+    name: "อะไหล่ยนต์",
+    data: ["อะไหล่ยนต์"],
+    image: "/images/Car-accessories2.png",
+  },
   { name: "จักรยาน", data: ["จักรยาน"], image: "/images/Bicycle2.png" },
-  { name: "เครื่องดนตรี", data: ["เครื่องดนตรี", "กีตาร์"], image: "/images/Music2.png" },
+  {
+    name: "เครื่องดนตรี",
+    data: ["เครื่องดนตรี", "กีตาร์"],
+    image: "/images/Music2.png",
+  },
   { name: "นาฬิกา", data: ["นาฬิกา"], image: "/images/Wrist-watch2.png" },
-  { name: "เสื้อผ้าและแฟชั่น", data: ["เสื้อผ้าและแฟชั่น"], image: "/images/Fasion2.png" },
+  {
+    name: "เสื้อผ้าและแฟชั่น",
+    data: ["เสื้อผ้าและแฟชั่น"],
+    image: "/images/Fasion2.png",
+  },
   { name: "รองเท้า", data: ["รองเท้า"], image: "/images/Shoes2.png" },
   { name: "กีฬา", data: ["กีฬา", "อุปกรณ์กีฬา"], image: "/images/Sport2.png" },
   { name: "กล้อง", data: ["กล้อง"], image: "/images/Camera2.png" },
   { name: "กระเป๋า", data: ["กระเป๋า"], image: "/images/Bag2.png" },
   { name: "เกมส์", data: ["เกมส์"], image: "/images/Game2.png" },
-  { name: "เบ็ดตกปลา เหยื่อ", data: ["เบ็ดตกปลา", "เหยื่อปลอม"], image: "/images/Bait2.png" },
+  {
+    name: "เบ็ดตกปลา เหยื่อ",
+    data: ["เบ็ดตกปลา", "เหยื่อปลอม"],
+    image: "/images/Bait2.png",
+  },
   { name: "โมเดล", data: ["โมเดล"], image: "/images/Model2.png" },
   { name: "แม่และเด็ก", data: ["แม่และเด็ก"], image: "/images/Toy2.png" },
   { name: "เกษตร", data: ["เกษตร"], image: "/images/Plant2.png" },
   { name: "ท่องเที่ยว", data: ["ท่องเที่ยว"], image: "/images/Travel2.png" },
-  { name: "สุขภาพและความงาม", data: ["สุขภาพและความงาม"], image: "/images/Beauty.png" },
+  {
+    name: "สุขภาพและความงาม",
+    data: ["สุขภาพและความงาม"],
+    image: "/images/Beauty.png",
+  },
   { name: "ชา กาแฟ", data: ["กาแฟ"], image: "/images/Coffee2.png" },
   { name: "การเรียน", data: ["การเรียน"], image: "/images/Education2.png" },
-]
+];
 
 const FullWidthGrid = (props: { keywords: Keyword[] }) => {
-  const keywords = props.keywords
+  const keywords = props.keywords;
   const classes = useStyles();
-  const keywordFilter = keywordTypes.map((type) => {
-    if (keywords.find((k) => type.data.includes(k.keyword))) { return type }
-  }).filter(x => x !== undefined)
+  const keywordFilter = keywordTypes
+    .map((type) => {
+      if (keywords.find((k) => type.data.includes(k.keyword))) {
+        return type;
+      }
+    })
+    .filter((x) => x !== undefined);
 
   const [expanded, setExpanded] = React.useState(false);
 
@@ -108,54 +161,76 @@ const FullWidthGrid = (props: { keywords: Keyword[] }) => {
     setExpanded(!expanded);
   };
   return (
-    <div className={classes.root} >
+    <div className={classes.root}>
       <CardContent>
-       <Grid container spacing={1} className={classes.gridCon}>
-        {keywordFilter.map((k: any, index ) => {
-          if (index >=6) return <></>
+        <Grid container spacing={1} className={classes.gridCon}>
+          {keywordFilter.map((k: any, index) => {
+            if (index >= 6) return <></>;
 
-          return <Grid item xs={4} md={3} sm={3} lg={2} key={index}>
-            <Paper className={classes.paper}>
-              <Image
-                className="img-category"
-                src={k.image}
-                alt=""
-                width={75}
-                height={75}
-              />
-              <p className={classes.textLink}>{k.name}</p>
-            </Paper>
-          </Grid >
-        })}
-      </Grid>
-     </CardContent>
+            return (
+              <Grid item xs={4} md={3} sm={3} lg={2} key={index}>
+                <Paper className={classes.paper}>
+                  <Link href="/#">
+                    <Image
+                      className="img-category"
+                      src={k.image}
+                      alt=""
+                      width={75}
+                      height={75}
+                    />
+
+                    <p className={classes.textLink}>{k.name}</p>
+                  </Link>
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </CardContent>
       <Collapse in={expanded} timeout="auto">
         <CardContent>
-        <Grid container spacing={1} className={classes.gridCon}>
-        {keywordFilter.map((k: any, index) => {
-          if (index <=5) return <></>
+          <Grid container spacing={1} className={classes.gridCon}>
+            {keywordFilter.map((k: any, index) => {
+              if (index <= 5) return <></>;
 
-          return <Grid item xs={4} md={3} sm={3} lg={2} key={index}>
-            <Paper className={classes.paper}>
-              <Image
-                className="img-category"
-                src={k.image}
-                alt=""
-                width={75}
-                height={75}
-              />
-              <p className={classes.textLink}>{k.name}</p>
-            </Paper>
-          </Grid >
-        })}
-      </Grid>
+              return (
+                <Grid item xs={4} md={3} sm={3} lg={2} key={index}>
+                  <Paper className={classes.paper}>
+                    <Link href="/#">
+                      <Image
+                        className="img-category"
+                        src={k.image}
+                        alt=""
+                        width={75}
+                        height={75}
+                      />
+
+                      <p className={classes.textLink}>{k.name}</p>
+                    </Link>
+                  </Paper>
+                </Grid>
+              );
+            })}
+          </Grid>
         </CardContent>
       </Collapse>
-      <p style={{ textAlign: 'center' }} className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })} onClick={handleExpandClick}  aria-expanded={expanded} aria-label="show more">ดูมากขึ้น <img style={{ margin: '-4px 4px',width: '17px',height: 'auto',}} src="/images/dropdown.png" /></p>
+      <p
+        style={{ textAlign: "center" }}
+        className={clsx(classes.expand, {
+          [classes.expandOpen]: expanded,
+        })}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+      >
+        ดูมากขึ้น{" "}
+        <img
+          style={{ margin: "-4px 4px", width: "17px", height: "auto" }}
+          src="/images/dropdown.png"
+        />
+      </p>
     </div>
   );
-}
+};
 
-export default FullWidthGrid
+export default FullWidthGrid;
