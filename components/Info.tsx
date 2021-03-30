6ef524897b74f11269b3afdeb5c350f8074fb053
James <64403343+jokerjames001@users.noreply.github.com>
2021-03-30 15:41:42 +0700

    Update Info.tsx

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,7 +3,7 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Image from "next/image";
-import { Button, Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     paper: {
-      padding: '0px 16px 0px 16px',
+      padding: "0px 16px 0px 16px",
       textAlign: "left",
       color: theme.palette.text.secondary,
       backgroundColor: "#f2f2f2",
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     spacingxmobile: {
-      marginBottom: '3em',
+      marginBottom: "3em",
       ["@media (width:375px)"]: {
         display: "contents",
       },
@@ -35,38 +35,70 @@ const useStyles = makeStyles((theme: Theme) =>
       color: "#00C900",
       fontSize: "1.5rem",
       padding: "0",
-      textAlign: 'center',
+      textAlign: "center",
     },
     textHeader: {
       fontSize: "18px",
       textAlign: "left",
       backgroundColor: "#00C660",
-      padding: "15px 10px 16px 10px",
+      padding: "1px 8px 1px 15px",
       color: " #fff",
-      margin:'-15px'
+      margin: "-15px",
     },
-    btnDis:{
-      color:'#00C660'
+    textHeader2: {
+      color: " #000",
+      marginTop: "0px",
+      marginLeft: "-15px",
+      marginRight: "-15px",
+      padding: "1px 8px 1px 15px",
+      fontSize: "18px",
+      textAlign: "left",
+      backgroundColor: "#d7dde1",
+      boxShadow: "none",
+      borderRadius:'0px'
+      // backgroundColor: "#E8EEF2",
     },
-    TextInfo:{
-      color:'#111',
-      marginTop:'1.5em'
+    btnDis: {
+      color: "#00C660",
     },
-    btnSell:{
-      backgroundColor: '#fff',
-      color: '#000',
-      float:'right',
-      padding: '3px 21px',
+    TextInfo: {
+      color: "#111",
+      marginTop: "1.5em",
     },
-    textSpan:{
-      paddingLeft:'10px'
+    btnSell: {
+      backgroundColor: "#fff",
+      color: "#000",
+      float: "right",
+      padding: "3px 21px",
     },
-    gridCenter:{
-      alignSelf:'center'
+    btnSell2: {
+      backgroundColor: "#00C660",
+      color: "#000",
+      float: "right",
+      padding: "3px 21px",
+    },
+    textSpan: {
+      paddingLeft: "10px",
+    },
+    gridCenter: {
+      alignSelf: "center",
+    },
+    grid: {
+      padding: "30px",
+    },
+    ptext: {
+      color: "#d7dde1",
+    },
+    gridMobile:{
+      display: 'flex',
+      ["@media (max-width:800px)"]: {
+        display: 'block !important',
+      },
     }
-
-}
-  )
+    // imgCorrect: {
+    //   marginTop: "5px !important",
+    // },
+  })
 );
 
 export default function AutoGrid() {
@@ -86,93 +118,121 @@ export default function AutoGrid() {
             </p>
           </Paper>
         </Grid>
-      </Grid>
-      <Grid container spacing={3} className={classes.spacingxmobile}>
-        <Grid item xs={12} lg={6} md={6}>
-          <Paper className={classes.paper}>
-            <Grid className={classes.gridCenter}>
-            <p className={classes.textHeader}>
-              ผู้ขาย<Button className={classes.btnSell}>ลงขาย</Button>
-            </p>
-            </Grid>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-             <span className={classes.textSpan}>ขั้นตอนง่าย ใครก็ลงขายได้</span>
-            </Typography>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-             <span className={classes.textSpan}> โปรโมทขายเพื่อเพิ่มโอกาศในการขายมากขึ้น</span>
-            </Typography>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-               <span className={classes.textSpan}>แชทกับลูกค้าโดยตรงเพื่อเพิ่มโอกาสในการปิดการขาย</span>
-            </Typography>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-               <span className={classes.textSpan}>มีระบบหลังบ้านในการจัดการรายการเพื่อโปรโมทสิ้นค้าได้ง่าย</span>
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} lg={6} md={6}>
-          <Paper className={classes.paper}>
-          <p className={classes.textHeader}>
-              ผู้ซื้อ
-            </p>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-               <span className={classes.textSpan}>สะดวกมั่นใจในการเลือกซื้อสินค้าที่หลากหลาย</span>
-            </Typography>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-               <span className={classes.textSpan}>พูดคุย สอบถามกับคนขายได้โดยตรงเพื่อความปรอดภัยในการซื้อ</span>
-            </Typography>
-            <Typography className={classes.TextInfo} color={"primary"}>
-              <Image
-                className="img-correct"
-                src="/images/CORRECT.png"
-                alt=""
-                width={20}
-                height={20}
-              />
-               <span className={classes.textSpan}>มีาทงเลือกมากมาย กับ ร้านค้าและสินค้ามากมายอัพเดทตลอดเวลา</span>
-            </Typography>
-          </Paper>
+        <Grid item lg={12} className={classes.gridMobile}>
+          <Grid item xs={12} lg={6} className={classes.grid}>
+            <Paper className={classes.textHeader}>
+              <p>
+                ผู้ขาย<Button className={classes.btnSell}>ลงขาย</Button>
+              </p>
+            </Paper>
+            <Paper
+              style={{ marginTop: "15px" }}
+              className={classes.textHeader2}
+            >
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  ขั้นตอนง่าย ใครก็ลงขายได้
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  โปรโมทขายเพื่อเพิ่มโอกาศในการขายมากขึ้น
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  แชทกับลูกค้าโดยตรงเพื่อเพิ่มโอกาสในการปิดการขาย
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  มีระบบหลังบ้านในการจัดการรายการเพื่อโปรโมทสิ้นค้าได้ง่าย
+                </span>
+              </p>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} lg={6} className={classes.grid}>
+            <Paper className={classes.textHeader}>
+              <p>
+                ผู้ซื้อ<Button className={classes.btnSell2}></Button>
+              </p>
+            </Paper>
+            <Paper
+              style={{ marginTop: "15px" }}
+              className={classes.textHeader2}
+            >
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  สะดวกมั่นใจในการเลือกซื้อสินค้าที่หลากหลาย
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  พูดคุย สอบถามกับคนขายได้โดยตรงเพื่อความปรอดภัยในการซื้อ
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p>
+                <Image
+                  src="/images/CORRECT.png"
+                  alt=""
+                  width={25}
+                  height={25}
+                />
+                <span className={classes.textSpan}>
+                  มีาทงเลือกมากมาย กับ ร้านค้าและสินค้ามากมายอัพเดทตลอดเวลา
+                </span>
+              </p>
+            </Paper>
+            <Paper className={classes.textHeader2}>
+              <p className={classes.ptext}>xxxxxx</p>
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
     </div>
