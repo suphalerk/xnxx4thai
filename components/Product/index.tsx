19e5ff2e1ed81442c9eb52cf6b0d64c8a9d90909
James <64403343+jokerjames001@users.noreply.github.com>
2021-05-21 16:08:41 +0700

    Update index.tsx

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -97,7 +97,7 @@ const Product = (props: any) => {
 
   return (
     <Layout title="Product | KaideeWa.com">
-      <Search2 />
+      {/* <Search2 /> */}
       <Container maxWidth="lg" className="container">
         <div className={classes.root}>
           <PicList data={content} />
